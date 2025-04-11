5543ba624cfe24c5259f6eb0848efec2a206a638
Victor Socinschi <chaosdaddy84@gmail.com>
2025-04-11 11:02:12 +0300

    Added lat and lng to City and map routes

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import styles from "./City.module.css";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -9,6 +9,12 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -18,8 +24,15 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-  return <h1>City</h1>;
-
+  return (
+    <>
+      <h1>City{id}</h1>;
+      <p>
+        Position: {lat} ,{lng}
+      </p>
+      ;
+    </>
+  );
   // return (
   //   <div className={styles.city}>
   //     <div className={styles.row}>
